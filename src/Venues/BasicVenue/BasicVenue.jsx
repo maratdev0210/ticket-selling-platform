cee40ede9e33109f98b366b7db20d2c3ec404645
maratdev0210 <maratforwork1@gmail.com>
2025-02-14 20:01:28 +0300

    keep up to date with the main branch

--- src/Venues/BasicVenue/BasicVenue.jsx
+++
@@ -1,9 +0,0 @@
-// A rectangle venue
-export default function BasicVenue() {
-  return (
-    <div className="_basic_venue w-[20vw] h-[60vh] bg-black/25 flex items-center justify-center">
-      {/* fill in the seats here */}
-      <p className="font-serif italic text-2xl">Scene</p>
-    </div>
-  );
-}
